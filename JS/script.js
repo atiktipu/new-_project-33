e8f48dae166f592e6bbd7e116651c915c3d0b033
atiktipu <mdatikulislamtipu@gmail.com>
2025-05-11 22:47:55 +0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+let playerOneName =document.querySelector(".playerOneNameInput")
+
+let startGameBtn =document.querySelector(".startGameBtn")
+
+let screenOneError = document.querySelector(".screenOneError")
+
+startGameBtn.addEventListener("click",function()  {
+   if (playerOneName any .value == "") {
+    screenOneError.style.dislpay="block"
+   }
+
+   
+})
+
+
